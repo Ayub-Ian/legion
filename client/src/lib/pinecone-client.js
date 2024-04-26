6b04a4d60dbc17e435f3ed4204644b32ab533669
ayub-ian <iayub.mia@gmail.com>
2024-04-26 15:30:58 +0300

    Llm and vector store configuration

---
+++ client/src/lib/pinecone-client.js
@@ -0,0 +1,60 @@
+import { Pinecone } from '@pinecone-database/pinecone'
+
+import { env } from "./config";
+import { delay } from "./utils";
+
+let pineconeClientInstance = null;
+
+// Create pineconeIndex if it doesn't exist
+async function createIndex(client, indexName) {
+  try {
+    await client.createIndex({
+      createRequest: {
+        name: indexName,
+        dimension: 1536,
+        metric: "cosine",
+      },
+    });
+    console.log(
+      `Waiting for ${process.env.INDEX_INIT_TIMEOUT} seconds for index initialization to complete...`
+    );
+    await delay(env.INDEX_INIT_TIMEOUT);
+    console.log("Index created !!");
+  } catch (error) {
+    console.error("error ", error);
+    throw new Error("Index creation failed");
+  }
+}
+
+// Initialize index and ready to be accessed.
+async function initPineconeClient() {
+  try {
+    const pineconeClient = new Pinecone();
+    await pineconeClient.init({
+      apiKey: env.PINECONE_API_KEY,
+      environment: env.PINECONE_ENVIRONMENT,
+    });
+    const indexName = env.PINECONE_INDEX_NAME;
+
+    const existingIndexes = await pineconeClient.listIndexes();
+
+    if (!existingIndexes.includes(indexName)) {
+      createIndex(pineconeClient, indexName);
+    } else {
+      console.log("Your index already exists. nice !!");
+    }
+
+    return pineconeClient;
+  } catch (error) {
+    console.error("error", error);
+    throw new Error("Failed to initialize Pinecone Client");
+  }
+}
+
+export async function getPineconeClient() {
+  if (!pineconeClientInstance) {
+    pineconeClientInstance = await initPineconeClient();
+  }
+
+  return pineconeClientInstance;
+}
---
+++ client/src/lib/vector-store.js
@@ -0,0 +1,42 @@
+import { env } from './config';
+import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
+import { PineconeStore } from 'langchain/vectorstores/pinecone';
+import { PineconeClient } from '@pinecone-database/pinecone';
+
+export async function embedAndStoreDocs(
+  client,
+  docs
+) {
+  /*create and store the embeddings in the vectorStore*/
+  try {
+    const embeddings = new OpenAIEmbeddings();
+    const index = client.Index(process.env.PINECONE_INDEX_NAME);
+
+    //embed the PDF documents
+    await PineconeStore.fromDocuments(docs, embeddings, {
+      pineconeIndex: index,
+      textKey: 'text',
+    });
+  } catch (error) {
+    console.log('error ', error);
+    throw new Error('Failed to load your docs !');
+  }
+}
+
+// Returns vector-store handle to be used a retrievers on langchains
+export async function getVectorStore(client) {
+  try {
+    const embeddings = new OpenAIEmbeddings();
+    const index = client.Index(process.env.PINECONE_INDEX_NAME);
+
+    const vectorStore = await PineconeStore.fromExistingIndex(embeddings, {
+      pineconeIndex: index,
+      textKey: 'text',
+    });
+
+    return vectorStore;
+  } catch (error) {
+    console.log('error ', error);
+    throw new Error('Something went wrong while getting vector store !');
+  }
+}
